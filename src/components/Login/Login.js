a2814f0116aa72cff5f8153676a75e3303611cdb
prathamesh5181 <prathameshpatil5181@gmail.com>
2023-09-23 18:51:29 +0530

    add the support for the context

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer=(state,action)=>{
 
@@ -31,9 +32,11 @@ const Login = (props) => {
   const [passwordState,dispatchPassword]=useReducer(PasswordReducer,{value:'',isValid:null});
   const {isValid:emailIsValid}=emailState;
   const {isValid:passwordIsValid} = passwordState; 
+
+  const authctx = useContext(AuthContext);
+
   useEffect(()=>{
     const indentifier=setTimeout(()=>{
-      console.log('hii');
       setFormIsValid(
         emailIsValid&&passwordIsValid
       );
@@ -70,7 +73,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
