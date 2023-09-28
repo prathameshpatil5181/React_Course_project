ae2329e1360d87a35db5be8f5c61e4210469f89f
prathamesh5181 <prathameshpatil5181@gmail.com>
2023-09-28 22:39:30 +0530

    added the custom hook

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://reactcourse-26d72-default-rtdb.firebaseio.com/task.json'
       );
 
       if (!response.ok) {
