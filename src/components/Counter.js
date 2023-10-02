014507d4fdf895f7454d1d4d58c32b9431447fed
prathamesh5181 <prathameshpatil5181@gmail.com>
2023-10-02 21:55:02 +0530

    frist commit on the redux branch

---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+import classes from "./Counter.module.css";
+import { useSelector,useDispatch } from "react-redux";
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state)=>state.showCounter);
+  const dispatch  = useDispatch();
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
+
+  const incrementHandler = ()=>{
+    dispatch({type:'increment'});
+  }
+
+  const increaseHandler = ()=>{
+    dispatch({type:'increase',amount:5});
+  }
+   const decrementHandler = ()=>{
+    dispatch({type:'decrement'});
+  }
+
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {showCounter&&<div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Custom Increment</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
